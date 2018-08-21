4381a7f2e644fd1a2d8800d45232ac0297c25476
Lucus Pettigrew <lapettigre@gmail.com>
2018-08-21 14:43:54 +0000

    adding the server code, render files

---
+++ app.js
@@ -0,0 +1,9 @@
+//just starting the stock server for now
+
+const http = require("http");
+
+http.createServer(function (req, res) {
+    // body...
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end();
+}).listen(process.env.PORT || 8080, process.env.IP || "0.0.0.0");
